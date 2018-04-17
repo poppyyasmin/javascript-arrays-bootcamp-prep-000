6da65af99cd788bc581ed41bdd9cbc6124ab9c5d
poppyyasmin <poppytait@gmail.com>
2018-04-17 10:41:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,3 +22,8 @@ function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
 function accessElementInArray(array, index){
   return array[index];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+  chocolateBars.shift();
+  return chocolateBars;
+}
