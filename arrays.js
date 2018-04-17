710420b16270019d44483d7b42224ba5b07abdb3
poppyyasmin <poppytait@gmail.com>
2018-04-17 10:43:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,3 +31,7 @@ function removeElementFromBeginningOfArray(chocolateBars){
   return chocolateBars.slice(1);
 }
 
+function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+  chocolateBars.pop();
+  return chocolateBars;
+}
